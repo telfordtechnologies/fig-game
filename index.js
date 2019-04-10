835343df528001216142eb32a3310382e954284e
qqqy <frogasaurus@hotmail.com>
2019-04-10 12:08:35 -0600

    server working

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
+import test, {drawPlayable} from "./lib/draw.js"
 
-function draw(){
-  // const canvasTest = document.getElementById('canvas');
-  // const ctx = canvas.getContext('2d');
+console.log(test)
 
-  // ctx.fillStyle = 'green';
-  // ctx.fillRect(25,25,100,100);
-  // ctx.clearRect(45,45,60,60);
-  // ctx.strokeRect(50,50,50,50);
+const canvas = document.getElementById("canvas");
+export const ctx = canvas.getContext("2d");
 
-
-
-  var canvas = document.getElementById('canvas')
-  if(canvas.getContext){
-    console.log("Hit")
-    var ctx = canvas.getContext('2d')
-    ctx.fillStyle = 'green';
-    ctx.beginPath();
-    ctx.moveTo(70,50);
-    ctx.lineTo(100,75);
-    ctx.lineTo(100,25);
-    ctx.fill()
-  } 
+drawLoop()
+function drawLoop(){
+  ctx.clearRect(0 , 0 , canvas.width, canvas.height);
+  drawPlayable();
+  requestAnimationFrame(drawLoop)
 }
+
+
+
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 *David's making a game in Javascript because he's bored.*
 
 Starting with this tutorial: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript
+
+4/8/2019 - tutorial is complete. Going to try for a little mock-up.
