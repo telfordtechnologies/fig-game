b5325a4e4c7567b1052570bcc89cc29d9d9b0f7d
qqqy <frogasaurus@hotmail.com>
2019-04-29 15:01:33 -0600

    tutorial complete

--- multiplayerServer.js
+++ multiplayerServer.js
@@ -8,6 +8,11 @@ const express = require("express");
 const app = express()
 const port = 8080;
 let serverNumber = 0;
+const WebSocketServer = require('websocket').server;
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+server.listen(80);
 
 app.use(express.json());
 
@@ -19,7 +24,26 @@ app.get("/api/getNumber" , (req, res) => {
   res.status(200).send({serverNumber: serverNumber})
 })
 
+app.use(express.static(__dirname))
+
 setInterval(() => {
   serverNumber++
 }, 5000);
 
+console.log("Creating Websocket Server...");
+const wsServer = new WebSocketServer({
+  httpServer: app,
+  autoAcceptConnections: true,
+})
+console.log("WebSocket Server Created!")
+
+wsServer.on("request" , (request) => {
+  console.log("Hey, something happened")
+})
+
+io.on("connection" , function (socket) {
+  socket.emit('news' , {hello: 'world'});
+  socket.on('my other event', function (data){
+    console.log(data);
+  });
+});
