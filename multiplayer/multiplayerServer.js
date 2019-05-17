5e6be65c3d5745e6bc34fbfa037f29f45f45a1c5
qqqy <frogasaurus@hotmail.com>
2019-05-17 14:58:55 -0600

    removed commented code from multiplayer

--- multiplayer/multiplayerServer.js
+++ multiplayer/multiplayerServer.js
@@ -1,14 +1,7 @@
-// var connect = require('connect');
-// var serveStatic = require('serve-static');
-// connect().use(serveStatic(__dirname)).listen(8080 , function(){
-//   console.log('Server running on 8080...')
-// })
-
 const express = require("express");
 const app = express()
 const port = 8080;
 let serverNumber = 0;
-// const WebSocketServer = require('websocket').server;
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
@@ -16,9 +9,6 @@ server.listen(port);
 
 app.use(express.json());
 
-// app.listen(port , () => {
-//   console.log(`${port} is our port in the storm.`)
-// })
 
 app.get("/api/getNumber" , (req, res) => {
   res.status(200).send({serverNumber: serverNumber})
@@ -28,20 +18,8 @@ app.use(express.static(__dirname))
 
 setInterval(() => {
   serverNumber++
-  // socket.emit("number" , {number: serverNumber})
   console.log(serverNumber)
 }, 1000);
-
-// console.log("Creating Websocket Server...");
-// const wsServer = new WebSocketServer({
-//   httpServer: app,
-//   autoAcceptConnections: true,
-// })
-// console.log("WebSocket Server Created!")
-
-// wsServer.on("request" , (request) => {
-//   console.log("Hey, something happened")
-// })
 
 let playersArr = [];
 
@@ -50,46 +28,11 @@ io.on("connection" , function (socket) {
   socket.on('my other event', function (data){
     console.log(data);
   });
-  // socket.on("number" , function (data){
-  //   socket.emit("number" , {number: serverNumber})
-  // });
   socket.on("update" , (data) => {
-    // console.log("INBOUND: " , data)
-    // if(!playersArr.find((player) => {
-    //   return player.id === data.id
-    // })){
-    //   playersArr.push(data)
-    //   socket.emit("update" , playersArr)
-    //   console.log("pushed" , playersArr)
-    // }
-    // else
-    // {
-    // playersArr.find(p => p.id === "TEST").x = serverNumber
     playersArr = [...playersArr.filter(p => p.id !== data.id) , data]
     socket.emit("update" , playersArr);
-    // console.log("updated")
   })
 });
 
-// var app = require('express')();
-// var http = require('http').createServer(app);
-// var io = require('socket.io')(http);
 
 
-
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// io.on('connection' , function(socket){
-//   socket.on('chat message' , function(msg){
-//     io.emit('chat message' , msg)
-//   })
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected')
-//   })
-// })
-
-// http.listen(8080, function(){
-//   console.log('listening on *:3000');
-// });
